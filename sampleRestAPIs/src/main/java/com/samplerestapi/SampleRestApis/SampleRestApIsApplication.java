f942d1a914eb09c185d0378e28e498bc03c4fa96
eppalapallysharath <eppalapallysharath@gmail.com>
2025-12-03 23:22:22 +0530

    first springboot app

---
+++ sampleRestAPIs/src/main/java/com/samplerestapi/SampleRestApis/SampleRestApIsApplication.java
@@ -0,0 +1,13 @@
+package com.samplerestapi.SampleRestApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestApIsApplication.class, args);
+	}
+
+}
---
+++ sampleRestAPIs/src/test/java/com/samplerestapi/SampleRestApis/SampleRestApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samplerestapi.SampleRestApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRestApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
